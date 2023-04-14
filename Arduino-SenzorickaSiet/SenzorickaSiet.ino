9352df13b10cd7b1af09bfe81032bc200bd2095b
MatusValko <72170259+MatusValko@users.noreply.github.com>
2023-04-14 11:30:30 +0200

    Add files via upload

--- Arduino-SenzorickaSiet/SenzorickaSiet.ino
+++ Arduino-SenzorickaSiet/SenzorickaSiet.ino
@@ -5,8 +5,6 @@
 #define FIREBASE_AUTH "8HYBrCaWCCqZ1tdRLxenn4RDOd3mn6kZKIWbPHUM"
 #define WIFI_SSID "Valko_von"
 #define WIFI_PASSWORD "matusnet64"
-//#define WIFI_SSID "HUAWEI-fkuf"
-//#define WIFI_PASSWORD "pomarance13"
 
 #define DHTPIN A0 //Teplota
 #define MQ7pin A1 //CO
@@ -78,7 +76,7 @@ void SetUpDB(String path) {
       Serial.println(firebaseData.dataPath() + " = " + "Lx");
     }
   if (Firebase.setString(firebaseData, path + "/Svetlomer/Velicina", "Intenzita osvetlenia")) {
-      Serial.println(firebaseData.dataPath() + " = " + "Osvetlenie");
+      Serial.println(firebaseData.dataPath() + " = " + "Intenzita osvetlenia");
     }
   //Senzor dymu
   if (Firebase.setString(firebaseData, path + "/Senzor dymu/Jednotka", "Ppm")) {
@@ -87,11 +85,11 @@ void SetUpDB(String path) {
   if (Firebase.setString(firebaseData, path + "/Senzor dymu/Velicina", "Dym")) {
       Serial.println(firebaseData.dataPath() + " = " + "Dym");
     }
-  //Oxid uhoľnatý
-  if (Firebase.setString(firebaseData, path + "/Detektor Co/Jednotka", "Ppm")) {
+  //Detektor CO
+  if (Firebase.setString(firebaseData, path + "/Detektor CO/Jednotka", "Ppm")) {
       Serial.println(firebaseData.dataPath() + " = " + "Ppm");
     }
-  if (Firebase.setString(firebaseData, path + "/Detektor Co/Velicina", "Oxid uhoľnatý")) {
+  if (Firebase.setString(firebaseData, path + "/Detektor CO/Velicina", "Oxid uhoľnatý")) {
       Serial.println(firebaseData.dataPath() + " = " + "Oxid uhoľnatý");
     }
 
@@ -102,11 +100,11 @@ void SetUpDB(String path) {
   if (Firebase.setString(firebaseData, path + "/Hlukomer/Velicina", "Intenzita zvuku")) {
         Serial.println(firebaseData.dataPath() + " = " + "Intenzita zvuku");
      }
-  //Detektor Co2
-  if (Firebase.setString(firebaseData, path + "/Detektor Co2/Jednotka", "Ppm")) {
+  //Detektor CO2
+  if (Firebase.setString(firebaseData, path + "/Detektor CO2/Jednotka", "Ppm")) {
         Serial.println(firebaseData.dataPath() + " = " + "Ppm");
     }
-  if (Firebase.setString(firebaseData, path + "/Detektor Co2/Velicina", "Oxid uhličitý")) {
+  if (Firebase.setString(firebaseData, path + "/Detektor CO2/Velicina", "Oxid uhličitý")) {
         Serial.println(firebaseData.dataPath() + " = " + "Oxid uhličitý");
      }
   //Detektor pohybu   
@@ -126,38 +124,54 @@ void SendData(String path,String nodeName,int counter){
   
 
   //TEPLOTA
-  int t = dht.readTemperature();
-  if (Firebase.setFloat(firebaseData, path + "/Teplomer/Hodnota", t)) {
+  int t;
+  while (true){
+    t = dht.readTemperature();
+    Serial.print(",Teplota ");
+    Serial.print(t);
+    if(!isnan(t) && t != 0){
+      break;
+    }
+  }
+  if (Firebase.setInt(firebaseData, path + "/Teplomer/Hodnota", t)) {
       Serial.println(firebaseData.dataPath() + " = " + t);
     }
   //VLHKOSŤ
-  int h = dht.readHumidity();
-  if (Firebase.setFloat(firebaseData, path + "/Vlhkomer/Hodnota", h)) {
+  int h;
+  while (true){
+    h = dht.readHumidity();
+    Serial.print(",VLHKOSŤ ");
+    Serial.print(h);
+    if(!isnan(h) && h != 0){
+      break;
+    }
+  }
+  if (Firebase.setInt(firebaseData, path + "/Vlhkomer/Hodnota", h)) {
       Serial.println(firebaseData.dataPath() + " = " + h);
     }
   //SVETLOMER
   int value = analogRead(GL5539);
-  if (Firebase.setFloat(firebaseData, path + "/Svetlomer/Hodnota",value)) {
+  if (Firebase.setInt(firebaseData, path + "/Svetlomer/Hodnota",value)) {
         Serial.println(firebaseData.dataPath() + " = " + value);
     }
   //SENZOR DYMU, Treshold = 400
   int sensorValue2 = analogRead(MQ2pin);
-  if (Firebase.setFloat(firebaseData, path + "/Senzor dymu/Hodnota", sensorValue2)) {
+  if (Firebase.setInt(firebaseData, path + "/Senzor dymu/Hodnota", sensorValue2)) {
         Serial.println(firebaseData.dataPath() + " = " + sensorValue2);
     }
   //HLUKOMER
   int randomNumber3 = random(100);
-  if (Firebase.setFloat(firebaseData, path + "/Hlukomer/Hodnota", randomNumber3)) {
+  if (Firebase.setInt(firebaseData, path + "/Hlukomer/Hodnota", randomNumber3)) {
         Serial.println(firebaseData.dataPath() + " = " + randomNumber3);
      }
-  //DETEKTOR Co
+  //DETEKTOR CO
   int sensorValue = analogRead(MQ7pin);
-  if (Firebase.setFloat(firebaseData, path + "/Detektor Co/Hodnota", sensorValue)) {
+  if (Firebase.setInt(firebaseData, path + "/Detektor CO/Hodnota", sensorValue)) {
         Serial.println(firebaseData.dataPath() + " = " + sensorValue);
     }
-  //DETEKTOR Co2
+  //DETEKTOR CO2
   int co2 = sensorValue /2;
-  if (Firebase.setFloat(firebaseData, path + "/Detektor Co2/Hodnota", co2)) {
+  if (Firebase.setInt(firebaseData, path + "/Detektor CO2/Hodnota", co2)) {
         Serial.println(firebaseData.dataPath() + " = " + co2);
      }  
   //POHYB
@@ -172,7 +186,7 @@ void SendData(String path,String nodeName,int counter){
     invert = 0;
   }
   
-  if(Firebase.setFloat(firebaseData, path + "/Detektor pohybu/Hodnota", invert)) {
+  if(Firebase.setInt(firebaseData, path + "/Detektor pohybu/Hodnota", invert)) {
         Serial.println(firebaseData.dataPath() + " = " + invert);
     }
 
@@ -183,8 +197,8 @@ void SendData(String path,String nodeName,int counter){
               ",\"Svetlomer\":{\"Hodnota\":" + String(value) + ", \"Jednotka\": \"Lux\", \"Velicina\": \"Intenzita osvetlenia\"}"+
               ",\"Senzor dymu\":{\"Hodnota\":" + String(sensorValue2) + ", \"Jednotka\": \"Ppm\", \"Velicina\": \"Dym\"}"+
               ",\"Hlukomer\":{\"Hodnota\":" + String(randomNumber3) + ", \"Jednotka\": \"Db\", \"Velicina\": \"Intenzita zvuku\"}"+
-              ",\"Detektor Co\":{\"Hodnota\":" + String(sensorValue) + ", \"Jednotka\": \"Ppm\", \"Velicina\": \"Oxid uhoľnatý\"}"+
-              ",\"Detektor Co2\":{\"Hodnota\":" + String(co2) + ", \"Jednotka\": \"Ppm\", \"Velicina\": \"Oxid uhličitý\"}"+
+              ",\"Detektor CO\":{\"Hodnota\":" + String(sensorValue) + ", \"Jednotka\": \"Ppm\", \"Velicina\": \"Oxid uhoľnatý\"}"+
+              ",\"Detektor CO2\":{\"Hodnota\":" + String(co2) + ", \"Jednotka\": \"Ppm\", \"Velicina\": \"Oxid uhličitý\"}"+
               ",\"Detektor pohybu\":{\"Hodnota\":" + String(invert) + ", \"Jednotka\": \"Pohyb\", \"Velicina\": \"Pohyb\"}"+
 
               ",\"Timestamp\":" + timestamp +"}";
